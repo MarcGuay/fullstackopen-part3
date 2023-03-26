3b685ce5e20c96cf7e6d1cc05c305c8537d6179d
Marc Guay <marc.guay@gmail.com>
2023-03-26 14:01:18 -0400

    Add production build of phonebook

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cors = require('cors')
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static('build'))
 
 morgan.token('postdata', function (req, res) { return JSON.stringify(req.body) })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :postdata'));
