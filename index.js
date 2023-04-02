aec975d743c4162f6b91e3a6c8ca106b5f1efda8
Marc Guay <marc.guay@gmail.com>
2023-04-02 11:12:57 -0400

    3.13: Phonebook database, step1

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
@@ -70,7 +71,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
