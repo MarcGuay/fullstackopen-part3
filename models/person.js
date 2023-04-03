5943021272a8c443cb61403ed6cde736dbd313e7
Marc Guay <marc.guay@gmail.com>
2023-04-03 17:40:35 -0400

    Add validation of minLength:3 to name field

--- models/person.js
+++ models/person.js
@@ -15,10 +15,12 @@ mongoose.connect(url)
   })
 
 const personSchema = new mongoose.Schema({
-  name: String,
+  name: {
+    type: String,
+    minLength: 3
+  },
   number: String,
 })
-
 
 personSchema.set('toJSON', {
   transform: (document, returnedObject) => {
